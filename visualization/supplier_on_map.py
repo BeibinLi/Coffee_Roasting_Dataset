fa67e72864ee1cbec7be700e32afa7d6282cdd13
zhourunlong <zhourunlongvector@gmail.com>
2023-08-14 14:56:59 -0700

    supplier_on_map modular

--- visualization/supplier_on_map.py
+++ visualization/supplier_on_map.py
@@ -16,29 +16,36 @@ def get_coordinates(city, country):
         lat, lng = results["raw"]["point"]["coordinates"]
         return lat, lng
 
-# Load the csv data into pandas dataframe
-df = pd.read_csv("database/supplier.csv")
+def get_city_counts():
+    # Load the csv data into pandas dataframe
+    df = pd.read_csv("database/supplier.csv")
 
-# Group by city and count the number of suppliers in each city
-city_counts = df.groupby(['country', 'city']).size().reset_index(name='supplier_count')
+    # Group by city and count the number of suppliers in each city
+    return df.groupby(['country', 'city']).size().reset_index(name='supplier_count')
 
-# Create a map centered around the world
-m = folium.Map(location=[20,0], zoom_start=2, tiles='cartodbpositron')
+def plot_map(city_counts):
+    # Create a map centered around the world
+    m = folium.Map(location=[20,0], zoom_start=2, tiles='cartodbpositron')
 
-# For each city in the dataframe, add a circle to the map
-for index, row in city_counts.iterrows():
-    coords = get_coordinates(row['city'], row['country'])
-    if coords:
-        folium.Circle(
-            location=coords,
-            radius=int(row['supplier_count']*10000),  # adjust multiplier as per your need
-            color='blue',
-            fill=True,
-            fill_color='blue',
-            fill_opacity=0.6,
-            popup=f"{row['city']}, {row['country']} ({row['supplier_count']} suppliers)"
-        ).add_to(m)
+    # For each city in the dataframe, add a circle to the map
+    for index, row in city_counts.iterrows():
+        coords = get_coordinates(row['city'], row['country'])
+        if coords:
+            folium.Circle(
+                location=coords,
+                radius=int(row['supplier_count']*10000),  # adjust multiplier as per your need
+                color='blue',
+                fill=True,
+                fill_color='blue',
+                fill_opacity=0.6,
+                popup=f"{row['city']}, {row['country']} ({row['supplier_count']} suppliers)"
+            ).add_to(m)
 
-# Save the map to an HTML file
-os.makedirs('images', exist_ok=True)
-m.save('images/supplier_on_map.html')
+    # Save the map to an HTML file
+    os.makedirs('images', exist_ok=True)
+    m.save('images/supplier_on_map.html')
+
+if __name__ == "__main__":
+    city_counts = get_city_counts()
+    plot_map(city_counts)
+    
