09f686e9e9ac2036e2b18b3f31a8a627ed084f38
zhourunlong <zhourunlongvector@gmail.com>
2023-08-14 14:46:37 -0700

    membership_duration modular

--- visualization/membership_duration.py
+++ visualization/membership_duration.py
@@ -2,26 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Load the customer data
-df = pd.read_csv('database/customer.csv')
+def get_data():
+    # Load the customer data
+    df = pd.read_csv('database/customer.csv')
 
-# Convert 'member_since' column to datetime format
-df['member_since'] = pd.to_datetime(df['member_since'])
+    # Convert 'member_since' column to datetime format
+    df['member_since'] = pd.to_datetime(df['member_since'])
 
-# Extract the year and month from the 'member_since' column
-df['membership_year_month'] = df['member_since'].dt.to_period('M')
+    # Extract the year and month from the 'member_since' column
+    df['membership_year_month'] = df['member_since'].dt.to_period('M')
 
-# Count the number of members who joined each month and year
-membership_counts = df['membership_year_month'].value_counts().sort_index()
+    # Count the number of members who joined each month and year
+    return df['membership_year_month'].value_counts().sort_index()
 
-# Plotting
-plt.figure(figsize=(15,7))
-membership_counts.plot(kind='bar', color='skyblue')
-plt.title('Duration of Membership (Monthly)')
-plt.xlabel('Year-Month')
-plt.ylabel('Number of Customers Joined')
-plt.xticks(rotation=45)
-plt.tight_layout()
+def plot(membership_counts):
+    plt.figure(figsize=(15,7))
+    membership_counts.plot(kind='bar', color='skyblue')
+    plt.title('Duration of Membership (Monthly)')
+    plt.xlabel('Year-Month')
+    plt.ylabel('Number of Customers Joined')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
 
-os.makedirs('images', exist_ok=True)
-plt.savefig('images/membership_duration.png')
+    os.makedirs('images', exist_ok=True)
+    plt.savefig('images/membership_duration.png')
+
+if __name__ == "__main__":
+    membership_counts = get_data()
+    plot(membership_counts)
